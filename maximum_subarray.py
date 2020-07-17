1b8b1d69a4d9f46000130289374b63ad2e474dc0
etienne <etnnth@gmail.com>
2020-07-17 15:36:45 +0200

    correct typo

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -3,7 +3,6 @@ class Solution:
         for i in range(1,len(nums)):
             if nums[i-1]>0:
                 nums[i]+=nums[i-1]
-            i+=1
         return max(nums)
 
 assert Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) == 6
